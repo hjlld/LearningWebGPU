6ca27fb2ba05b726a4d341eb0e13ce670357289d
hjlld <vickihjl@qq.com>
2019-12-26 18:59:38 +0800

    initial commit

---
+++ Lesson1_Triangle_and_square/Code/src/shader/vertex.glsl.ts
@@ -0,0 +1,17 @@
+export default `
+#version 450
+
+layout(binding = 0) uniform Uniforms {
+
+  mat4 uPMatrix;
+  mat4 uMVMatrix;
+
+};
+
+layout(location = 0) in vec3 aVertexPosition;
+
+void main() {
+    // gl_Position = uPMatrix * uMVMatrix * vec4(aVertexPosition, 1.0);
+    gl_Position = uPMatrix * uMVMatrix * vec4(aVertexPosition, 1.0);
+}
+`;
---
+++ Lesson2_Add_Color/Code/src/main.ts
@@ -0,0 +1,107 @@
+import { App } from './app';
+import vxCode from './shader/vertex.glsl';
+import fxCode from './shader/fragment.glsl'
+import { PerspectiveCamera, Matrix4, Vector3 } from 'three';
+
+const triangleVertex = new Float32Array( [
+
+    // position         // color
+    0.0,  1.0,  0.0,    1.0, 0.0, 0.0, 1.0,
+   -1.0, -1.0,  0.0,    0.0, 1.0, 0.0, 1.0,
+    1.0, -1.0,  0.0,    0.0, 0.0, 1.0, 1.0
+
+] );
+
+const triangleVertexPositon = new Float32Array( [
+  
+    0.0,  1.0,  0.0, 
+   -1.0, -1.0,  0.0, 
+    1.0, -1.0,  0.0, 
+
+] );
+
+const triangleVertexColor = new Float32Array( [
+
+    1.0, 0.0, 0.0, 1.0,
+    0.0, 1.0, 0.0, 1.0,
+    0.0, 0.0, 1.0, 1.0
+
+] );
+
+const triangleIndex = new Uint32Array( [ 0, 1, 2 ] );
+
+const triangleMVMatrix = new Matrix4().makeTranslation( -1.5, 0.0, -7.0 );
+
+const squareVertex = new Float32Array( [
+
+    // position         // color
+     1.0,  1.0,  0.0,   0.5, 0.5, 1.0, 1.0,
+    -1.0,  1.0,  0.0,   0.5, 0.5, 1.0, 1.0,
+     1.0, -1.0,  0.0,   0.5, 0.5, 1.0, 1.0,
+    -1.0, -1.0,  0.0,   0.5, 0.5, 1.0, 1.0
+
+] );
+
+const squareVertexPosition = new Float32Array( [
+
+     1.0,  1.0,  0.0,
+    -1.0,  1.0,  0.0,
+     1.0, -1.0,  0.0,
+    -1.0, -1.0,  0.0
+
+] );
+
+const squareVertexColor = new Float32Array( [
+
+    0.5, 0.5, 1.0, 1.0,
+    0.5, 0.5, 1.0, 1.0,
+    0.5, 0.5, 1.0, 1.0,
+    0.5, 0.5, 1.0, 1.0,
+
+] );
+
+const squareIndex = new Uint32Array( [ 0, 1, 2, 1, 2, 3 ] );
+
+const squareMVMatrix = new Matrix4().makeTranslation( 1.5, 0.0, -7.0 );
+
+let main = async () => {
+
+    let camera = new PerspectiveCamera( 45, document.body.clientWidth / document.body.clientHeight, 0.1, 100 );
+
+    let pMatrix = camera.projectionMatrix;
+
+    let triangleUniformBufferView = new Float32Array( pMatrix.toArray().concat( triangleMVMatrix.toArray() ) );
+
+    let squareUniformBufferView = new Float32Array( pMatrix.toArray().concat( squareMVMatrix.toArray() ) );
+
+    let backgroundColor = { r: 0, g: 0, b: 0, a: 1.0 };
+
+    let app = new App();
+
+    app.CreateCanvas( document.body )
+
+    await app.InitWebGPU();
+
+    app.InitRenderPass( backgroundColor );
+
+    // app.InitPipeline( vxCode, fxCode );
+
+    app.InitPipelineWitMultiBuffers( vxCode, fxCode );
+
+    // app.InitGPUBuffer( triangleVertex, triangleIndex, triangleUniformBufferView );
+
+    app.InitGPUBufferWithMultiBuffers( triangleVertexPositon, triangleVertexColor, triangleIndex, triangleUniformBufferView )
+
+    app.Draw( triangleIndex.length );
+
+    // app.InitGPUBuffer( squareVertex, squareIndex, squareUniformBufferView );
+
+    app.InitGPUBufferWithMultiBuffers( squareVertexPosition, squareVertexColor, squareIndex, squareUniformBufferView )
+
+    app.Draw( squareIndex.length );
+
+    app.Present();
+
+}
+
+window.addEventListener( 'DOMContentLoaded', main );
